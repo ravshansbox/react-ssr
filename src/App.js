5829b2fe01b031773132ea64ea8d649b3f9ad39a
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-21 16:47:28 +0300

    implement `Navbar`

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Navbar } from './Navbar';
 import { Routes, Route } from 'react-router';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" Component={Home} />
-      <Route path="/about" Component={About} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+      </Routes>
+    </>
   );
 };
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router';
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
