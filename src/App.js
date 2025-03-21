94cc1762ced4a29c13adb08a8be7c6489ac1fd20
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-21 16:43:38 +0300

    add `react-router`

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { Routes, Route } from 'react-router';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+
 export const App = () => {
-  return <h1>Hello React</h1>;
+  return (
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/about" Component={About} />
+    </Routes>
+  );
 };
--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 import http from 'node:http';
 import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router';
 import { wrapContent } from './wrapContent';
 import { App } from './App';
 
 const server = http.createServer();
 
-server.on('request', (_request, response) => {
+server.on('request', (request, response) => {
   response.setHeader('content-type', 'text/html');
   const title = 'React SSR';
-  const content = renderToString(<App />);
+  const content = renderToString(
+    <StaticRouter location={request.url}>
+      <App />
+    </StaticRouter>,
+  );
   response.end(wrapContent({ title, content }));
 });
 
